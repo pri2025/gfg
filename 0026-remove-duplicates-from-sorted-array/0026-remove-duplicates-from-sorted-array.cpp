6ee1db579f86a94e3293b92c07475d76d5bb05a3
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-07 11:26:36 +0530

    Time: 10 ms (39.78%), Space: 21 MB (64.59%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -7,10 +7,7 @@ public:
         }
         int j =1;
         for(int i=0; i<nums.size()-1; i++){
-            if(nums[i] == nums[i+1]){
-
-            }
-            else{
+            if(nums[i] != nums[i+1]){
                 nums[j] = nums[i+1];
                 j++;
             }
