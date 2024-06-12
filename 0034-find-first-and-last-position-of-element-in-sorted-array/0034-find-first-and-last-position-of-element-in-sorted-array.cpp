e2a006120f009c9a39d447262036d6d7bf0d6e10
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-12 20:01:59 +0530

    Time: 0 ms (100%), Space: 16.1 MB (12.81%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,20 +1,63 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
+    int firstOcc(vector<int>& nums, int target){
         int first = -1;
-        int last= -1;
-        for(int i =0; i<nums.size(); i++){
-            if(nums[i] == target){
-                if(first == -1){
-                    first = i;
-                }
-                last = i;
+        int i = 0;
+        int j = nums.size() - 1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(nums[mid] == target){
+                first = mid;
+                j = mid-1;
             }
-            
+            else if(nums[mid]< target){
+                i = mid+1;
+            }
+            else{
+                j = mid-1;
+            }
         }
-        ans.push_back(first);
-        ans.push_back(last);
-        return ans;
+        return first;
+    }
+    int lastOcc(vector<int> &nums, int target){
+        int last = -1;
+        int i = 0;
+        int j = nums.size() - 1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(nums[mid] == target){
+                last = mid;
+                i = mid+1;
+            }
+            else if(nums[mid] < target){
+                i = mid+1;
+            }
+            else{
+                j = mid-1;
+            }
+        }
+        return last;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        int first = firstOcc(nums,target);
+        int last = lastOcc(nums,target);
+        return{first,last};
+
     }
 };
+
+//int first = -1;
+        // int last= -1;
+        // for(int i =0; i<nums.size(); i++){
+        //     if(nums[i] == target){
+        //         if(first == -1){
+        //             first = i;
+        //         }
+        //         last = i;
+        //     }
+            
+        // }
+        // ans.push_back(first);
+        // ans.push_back(last);
+        // return ans;
