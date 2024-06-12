fab1a14d104dfc00d96c0c6f1a49847f309a9c65
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-12 14:06:52 +0530

    Time: 23 ms (82.46%), Space: 29.9 MB (66.32%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    // int bs(vector<int> &nums, int target,int i, int j){
+    //    if(i > j){
+    //     return -1;
+    //    }
+    //     int mid = (i + (j-i+1))/2;
+
+    //     if(target == nums[mid]){
+    //         return mid;
+    //     }
+    //     else if(target < nums[mid]){
+    //         bs(nums,target, i, mid-1);
+    //     }
+    //     else{
+    //         bs(nums,target,mid+1, j);
+    //     }
+    //     return -1;
+    // }
+    
+
+    int search(vector<int>& nums, int target) {
+        int i = 0;
+        int j = nums.size()-1;
+        while( i <= j){
+            int mid = (j+i)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                j = mid-1;
+            }
+            else{
+                i = mid + 1;
+            }
+        }
+        return -1;
+
+        // int result = bs(nums, target, i, j);
+        // return result;
+        
+
+    }
+};
