2f9cd26a84fd8d9a5c4c484eec48d979e1646664
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-15 23:10:52 +0530

    Time: 27 ms (20.32%), Space: 15.7 MB (13.35%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        set<char> st;
+        int i=0;
+         int j = 0;
+         int len = 0;
+        for(i = 0; i< s.length(); i++){
+            while(st.find(s[i]) != st.end() && j <= i){ //abcabcbb
+                st.erase(s[j]);
+                j++;
+            }
+            st.insert(s[i]);
+            len = max(len, i-j+1);
+        }
+        return len;
+    }
+};
+
+
+//string ans = ""; 
+        // string temp = "";
+        // if(s == " "){
+        //     return 1;
+        // }
+        // for(int i =0 ;i<s.length() ; i++){
+        //     if(mpp.find(s[i]) == mpp.end()){
+        //         mpp[s[i]] = 1;
+        //         temp = temp+s[i];
+        //     }
+        //     else if(ans.empty()){
+        //         ans =temp;
+        //         temp = "";
+        //         if(s[i] != s[i-1]){
+        //             temp = temp + s[i-1];
+        //         }
+        //         //mpp[s[i]]++;
+        //         temp = temp+s[i];
+        //     }
+        //     else{
+        //         if(temp.length() > ans.length()){
+        //             ans = temp;
+        //         }
+        //         for(int i = 0; i<s.length(); i++){
+        //             mpp[s[i]] = 0;
+        //         }
+        //         temp = "";
+        //         temp = temp+s[i];
+        //     }
+        // }
+        // if(!temp.empty() && temp.length() > ans.length()){
+        //     return temp.length();
+        // }
+        // return ans.length();
