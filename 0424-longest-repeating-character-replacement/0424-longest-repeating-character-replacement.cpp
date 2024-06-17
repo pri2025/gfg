307f8e88db61a4b5703baafffd070cb5abd392a9
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-17 12:37:18 +0530

    Time: 5 ms (81.14%), Space: 8 MB (95.7%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,27 +2,47 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int len = 0;
-        for(int i = 0; i<s.length() ; i++){
-            int mp[26] = {0};   int maxf = 0;
-            int change=0;
-            for(int j = i; j<s.length(); j++){
-                mp[s[j] - 'A']++;
-                maxf = max(maxf, mp[s[j] - 'A']);
-                change = (j-i+1)- maxf; // abhi jitni substring hai usmein
-                //kitna change kr skte
-                if(change <= k){ //agr valid hai to maxlen cal krte raho else break
-                    len = max(len, j-i+1);
+        int i = 0, j = 0, maxf = 0;
+        int mp[26] = {0};
+        while(j <s.length()){
+            mp[s[j] - 'A']++;
+            maxf = max(maxf,mp[s[j] - 'A']);
+            
+            while((j-i+1) - maxf > k){
+                mp[s[i] - 'A']--; 
+                maxf = 0;
+                for(int k = 0; k<26 ; k++){
+                    maxf = max(maxf, mp[k]);
                 }
-                else{
-                    break;
-                }
+                i++;
             }
+
+            if((j-i+1) - maxf <= k){
+                len = max(len, (j-i+1));
+            }
+            j++; 
         }
         return len;
     }
          
 };
-
+// for(int i = 0; i<s.length() ; i++){
+        //     int mp[26] = {0};   int maxf = 0;
+        //     int change=0;
+        //     for(int j = i; j<s.length(); j++){
+        //         mp[s[j] - 'A']++;
+        //         maxf = max(maxf, mp[s[j] - 'A']);
+        //         change = (j-i+1)- maxf; // abhi jitni substring hai usmein
+        //         //kitna change kr skte
+        //         if(change <= k){ //agr valid hai to maxlen cal krte raho else break
+        //             len = max(len, j-i+1);
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return len;
 
 
 //while(j <n){
