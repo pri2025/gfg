12e87f862985145ff7e2ffcc323e02eef55989d7
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-20 17:16:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Postfix to Prefix Conversion/postfix-to-prefix-conversion.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    stack<string> st;
+    string postToPre(string s) {
+        // Write your code here
+        for(auto it : s){
+            if(isalpha(it) || (it >= '0' && it<= '9')){
+                st.push(string(1,it));
+            }
+            else{
+                string A = st.top(); st.pop();
+                string B = st.top(); st.pop();
+                st.push(it + B + A);
+            }
+        }
+        return st.top();
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        string postfix;
+        cin >> postfix;
+
+        // char marker; cin >> marker;
+
+        Solution obj;
+        cout << obj.postToPre(postfix) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends
