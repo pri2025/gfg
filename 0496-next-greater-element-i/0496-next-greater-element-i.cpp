65771a3e28c7d6db68d4bb97531b454671f9bffd
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-21 12:32:20 +0530

    Time: 5 ms (45.59%), Space: 11.5 MB (62.34%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,24 +2,46 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int> res;
-        
-        for(int i=0; i<nums1.size(); i++){
-            int flag = 0;
-            for(int j= 0; j<nums2.size(); j++){
-                if(nums1[i] == nums2[j]){
-                    for(int k = j; k < nums2.size(); k++){
-                        if(nums2[k] > nums2[j]){
-                            flag = 1;
-                            res.push_back(nums2[k]);
-                            break;
-                        }
-                    }
-                    if(flag ==0){
-                        res.push_back(-1);
-                    }
-                }
+        stack<int> st;
+        map<int,int> mp;
+
+        for(int num : nums2){
+            while(!st.empty() && st.top() < num){
+                mp[st.top()] = num;
+                st.pop();
             }
+            st.push(num);
         }
+
+        while(!st.empty()){
+            mp[st.top()] = -1;
+            st.pop();
+        }
+
+        for(int num : nums1){
+            res.push_back(mp[num]);
+        }
+
         return res;
+    
     }
 };
+
+//for(int i=0; i<nums1.size(); i++){
+        //     int flag = 0;
+        //     for(int j= 0; j<nums2.size(); j++){
+        //         if(nums1[i] == nums2[j]){
+        //             for(int k = j; k < nums2.size(); k++){
+        //                 if(nums2[k] > nums2[j]){
+        //                     flag = 1;
+        //                     res.push_back(nums2[k]);
+        //                     break;
+        //                 }
+        //             }
+        //             if(flag ==0){
+        //                 res.push_back(-1);
+        //             }
+        //         }
+        //     }
+        // }
+        // return res;
