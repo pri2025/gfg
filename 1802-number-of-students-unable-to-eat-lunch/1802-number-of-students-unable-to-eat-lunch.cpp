4f4652b3d7a3690a786c6b239d16d5f759acaa6b
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-29 23:57:59 +0530

    Time: 0 ms (100%), Space: 10.9 MB (38.59%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
@@ -2,31 +2,53 @@ class Solution {
 public:
     stack<int> st; queue<int> q;
     int countStudents(vector<int>& stud, vector<int>& sw) {
+        int c1 = 0, c2= 0;
         for(int i =0; i < stud.size(); i++){
-            q.push(stud[i]);
+            //q.push(stud[i]);
+            if(stud[i] == 1)
+            c1++;//1
+            else c2++;//0
         }
 
         for(int i = sw.size()-1 ; i >= 0; i--){
             st.push(sw[i]);
         }
-        
-        int cnt = stud.size();
-        int a = 0;
-        while(!q.empty() && a < q.size()){
-            if(st.top() == q.front()){
-                st.pop();
-                q.pop();
-                cnt--;
-                a=0;
+        for(int i : sw){
+            if(i == 0 && c2 == 0){
+                return c1;
+            }
+            if(i == 1 && c1 == 0){
+                return c2;
+            }
+            if(i == 0){
+                c2--;
             }
             else{
-                int x = q.front();
-                q.pop();
-                q.push(x);
-                a++;
+                c1--;
             }
         }
-        return cnt;
+
+        return 0;
+        
     }
+
+    //     int cnt = stud.size();
+    //     int a = 0;
+    //     while(!q.empty() && a < q.size()){
+    //         if(st.top() == q.front()){
+    //             st.pop();
+    //             q.pop();
+    //             cnt--;
+    //             a=0;
+    //         }
+    //         else{
+    //             int x = q.front();
+    //             q.pop();
+    //             q.push(x);
+    //             a++;
+    //         }
+    //     }
+    //     return cnt;
+    // }
     
 };
