9ad60e63e4045abbadc1e3e0dd410feb2d350ff2
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-29 14:38:50 +0530

    Time: 0 ms (100%), Space: 10 MB (25.4%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
@@ -3,24 +3,42 @@ public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         if(tickets[k] ==0 ) return 0;
         int cnt = 0;
+        queue<int> q;
 
         for(int i =0; i<tickets.size(); i++){
-            if(i <= k){
-                if(tickets[i] <= tickets[k]){
-                    cnt = cnt+ tickets[i];
-                }
-                else if(tickets[i] > tickets[k]){
-                    cnt = cnt + tickets[k];
-                }
-            }
-            else{
-                cnt += min(tickets[i], tickets[k]-1);
-            }
+            q.push(i);
         }
+        while(!q.empty()){
+            int x = q.front();
+            q.pop();
+            if(tickets[x] >0){
+                tickets[x]--;
+                cnt++;
+            }
+        if(tickets[x] !=0){
+            q.push(x);
+        }
+        if(x == k && tickets[x] == 0){
+            break;
+        }
+        }
+        
         return cnt;
     }
 };
-
+//for(int i =0; i<tickets.size(); i++){
+        //     if(i <= k){
+        //         if(tickets[i] <= tickets[k]){
+        //             cnt = cnt+ tickets[i];
+        //         }
+        //         else if(tickets[i] > tickets[k]){
+        //             cnt = cnt + tickets[k];
+        //         }
+        //     }
+        //     else{
+        //         cnt += min(tickets[i], tickets[k]-1);
+        //     }
+        // }
 //while(tickets[k] > 0){
         //     for(int i=0 ; i< tickets.size(); i++){
         //         if(tickets[i] >0){
