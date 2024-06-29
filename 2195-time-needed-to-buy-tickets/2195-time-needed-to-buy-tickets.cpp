2e7941c0664111cbbb62b8cfcb5105b7529e2488
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-29 14:19:34 +0530

    Time: 0 ms (100%), Space: 9.3 MB (89.7%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
@@ -4,17 +4,31 @@ public:
         if(tickets[k] ==0 ) return 0;
         int cnt = 0;
 
-        while(tickets[k] > 0){
-            for(int i=0 ; i< tickets.size(); i++){
-                if(tickets[i] >0){
-                    tickets[i]--;
-                    cnt++;
+        for(int i =0; i<tickets.size(); i++){
+            if(i <= k){
+                if(tickets[i] <= tickets[k]){
+                    cnt = cnt+ tickets[i];
                 }
-                if(tickets[k] == 0){
-                    break;
+                else if(tickets[i] > tickets[k]){
+                    cnt = cnt + tickets[k];
                 }
+            }
+            else{
+                cnt += min(tickets[i], tickets[k]-1);
             }
         }
         return cnt;
     }
 };
+
+//while(tickets[k] > 0){
+        //     for(int i=0 ; i< tickets.size(); i++){
+        //         if(tickets[i] >0){
+        //             tickets[i]--;
+        //             cnt++;
+        //         }
+        //         if(tickets[k] == 0){
+        //             break;
+        //         }
+        //     }
+        // }
