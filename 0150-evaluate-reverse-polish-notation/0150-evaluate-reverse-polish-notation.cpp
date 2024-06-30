c0cfa68ff26576ce82aa30a3bf4728fcae101b2b
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-06-30 16:51:07 +0530

    Time: 6 ms (76.51%), Space: 15.4 MB (86.13%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,60 +1,30 @@
 class Solution {
 public:
-
-   int evalRPN(vector<string>& tokens) {
-        stack<int> stk;
-        for(string str : tokens) {
-            if(str == "+" || str == "-" || str == "*" || str == "/") {
-                int val1 = stk.top();
-                stk.pop();
-                int val2 = stk.top();
-                stk.pop();
-                int result;
-                if(str == "+") {
-                    result = val2 + val1;
+    stack<int> st;
+    int evalRPN(vector<string>& s) {
+        for(string i : s){
+            if(i == "+" || i == "-" ||i== "*"|| i =="/"){
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
+                if(i == "+"){
+                    st.push(a+b);
                 }
-                else if(str == "-") {
-                    result = val2 - val1;
+                else if(i =="-"){
+                    st.push(b-a);
                 }
-                else if(str == "*") {
-                    result = val2 * val1;
+                else if(i== "*"){
+                    st.push(a*b);
                 }
-                else if(str == "/") {
-                    result = val2 / val1;
+                else if(i == "/"){
+                    st.push(b/a);
                 }
-                stk.push(result);
             }
-            else {
-                stk.push(stoi(str));
+            else{
+                st.push(stoi(i));
             }
-        }
-        return stk.top();
-    }
-    // stack<int> st;
-    // int evalRPN(vector<string>& s) {
-    //     for(string i : s){
-    //         if(i == '+' || i == '-' ||i== '*'|| i ='/'){
-    //             int a = st.top(); st.pop();
-    //             int b = st.top(); st.pop();
-    //             if(s[i] == '+'){
-    //                 st.push(a+b);
-    //             }
-    //             else if(s[i] =='-'){
-    //                 st.push(b-a);
-    //             }
-    //             else if(s[i] == '*'){
-    //                 st.push(a*b);
-    //             }
-    //             else{
-    //                 st.push(a/b);
-    //             }
-    //         }
-    //         else{
-    //             st.push(stoi(i));
-    //         }
             
-    //     }
-    //     return st.top();
+        }
+        return st.top();
 
-    // }
+    }
 };
