9c8a33a32d066d156a667b4b29675b5d226c4f69
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-07-04 00:18:34 +0530

    Time: 102 ms (14.79%), Space: 47.5 MB (15.46%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        map<char,int> mp;
+        for(int i=0; i<tasks.size(); i++){
+            mp[tasks[i]]++;
+        }
+
+        priority_queue<int, vector<int>> pq;
+
+        for(auto i : mp){
+            pq.push(i.second);
+        }
+        int r = 0;
+        while(!pq.empty()){
+            vector<int> tmp;
+            int time = 0;
+            for(int i = 0; i<=n; i++){
+                if(!pq.empty()){
+                    tmp.push_back(pq.top() -1);//cnt decrease krke
+                    pq.pop();
+                    time++;
+                }
+            }
+            //push back to pq
+            for(auto i : tmp){
+                if(i) pq.push(i); // not zero
+            }
+             if(pq.empty()){
+                r += time;
+            }
+            else{
+                r += n+1;
+            }
+        }
+        return r;
+    }
+};
