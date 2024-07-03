745e06d3624322840bb6290d3051772d09a86383
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-07-03 15:15:49 +0530

    Time: 86 ms (75.94%), Space: 59.8 MB (72.95%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        if(nums.size() == 1){
-            return nums[0];
+      priority_queue<int, std::vector<int> , greater<int>> minheap(nums.begin(), nums.begin() + k);
+
+      for(int i = k; i< nums.size(); i++){ //from i =k because heap has k elements
+        if(nums[i] >= minheap.top()){
+            minheap.pop();
+            minheap.push(nums[i]);
         }
-        sort(nums.begin(),nums.end());
-        int s = nums[nums.size() -1];
-        int cnt = 1;
-        for(int i =nums.size()-2; i>=0;i--){
-            if(cnt == k){
-                return s;
-            }
-            else if(nums[i] <= s){
-                s = nums[i];
-                cnt++;
-            }
-        }
-        return s;
+      }
+      return minheap.top();
     }
 };
+
+  // if(nums.size() == 1){
+        //     return nums[0];
+        // }
+        // sort(nums.begin(),nums.end());
+        // int s = nums[nums.size() -1];
+        // int cnt = 1;
+        // for(int i =nums.size()-2; i>=0;i--){
+        //     if(cnt == k){
+        //         return s;
+        //     }
+        //     else if(nums[i] <= s){
+        //         s = nums[i];
+        //         cnt++;
+        //     }
+        // }
+        // return s;
