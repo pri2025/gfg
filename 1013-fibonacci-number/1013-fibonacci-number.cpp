6c36581e4465cb30c1e8c12863a64dda391b5bc6
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-07-09 13:48:16 +0530

    Time: 0 ms (100%), Space: 7.2 MB (34.11%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
 
-    int fn(int n, unordered_map<int,int>& mp){
-        if(n== 0 || n== 1) return n;
+    // int fn(int n, unordered_map<int,int>& mp){
+    //     if(n== 0 || n== 1) return n;
 
-        if(mp.find(n) != mp.end()) return mp[n];
-        mp[n] = fn(n-1,mp) + fn(n-2, mp);
-        return mp[n];
-    }
+    //     if(mp.find(n) != mp.end()) return mp[n];
+    //     mp[n] = fn(n-1,mp) + fn(n-2, mp);
+    //     return mp[n];
+    // }
     int fib(int n) {
-        unordered_map<int,int> mp;
-        return fn(n,mp);
+        // unordered_map<int,int> mp;
+        // return fn(n,mp);
+        if(n==0 || n== 1)return n;
+        vector<int> table(n+1,-1);
+        table[0] = 0; table[1] = 1;
+
+        for(int i = 2; i<=n; i++){
+            table[i] = table[i-1] + table[i-2];
+        }
+        return table[n];
     }
 };
 
