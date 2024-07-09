672d0110446dcca5f53408b443cbb04f421821af
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-07-09 13:44:11 +0530

    Time: 0 ms (100%), Space: 7.6 MB (11.83%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n == 0){
-            return 0;
-        }
-        if(n == 1){
-            return 1;
-        }
 
-        return fib(n-1) + fib(n-2);
+    int fn(int n, map<int,int>& mp){
+        if(n== 0 || n== 1) return n;
+
+        if(mp.find(n) != mp.end()) return mp[n];
+        mp[n] = fn(n-1,mp) + fn(n-2, mp);
+        return mp[n];
+    }
+    int fib(int n) {
+        map<int,int> mp;
+        return fn(n,mp);
     }
 };
+
+//  if(n ==0)
+//         return 0;
+//         if(n == 1)
+//         return 1;
+//         return fib(n-1) + fib(n-2); 
