dd98a4cee8e64aaa24690a82e495341bf4f94612
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-07-18 15:39:25 +0530

    Time: 0 ms (100%), Space: 16.4 MB (21.39%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int,int>> q;
+        int fresh = 0;
+        for(int i = 0; i<grid.size(); i++){
+            for(int j=0; j<grid[i].size();j++){
+                if(grid[i][j] == 2){
+                    q.push({i,j});
+                }
+                else if(grid[i][j] == 1){
+                    fresh++;
+                }
+            }
+        }
+        if(fresh == 0) return 0;
+
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        //to move l,r,up,down
+        int t = -1;
+        while(!q.empty()){
+            t++;
+            int n = q.size();
+            for(int i = 0; i <n; i++){
+                pair<int,int> cell = q.front(); q.pop();
+
+                for(auto dir: directions){
+                    int x = cell.first + dir.first;
+                    int y = cell.second + dir.second;
+
+                    if( (x >=0 && x <grid.size()) && (y >=0 && y<grid[0].size()) && 
+                        grid[x][y] == 1){
+                            grid[x][y] = 2;
+                            q.push({x,y});
+                            fresh--;
+                        }
+                }
+
+            }
+        }
+        if(fresh == 0) return t;
+        return -1;
+    }
+};
