ac5b784fbe16fbf58fa9d0374b2b6ec84c7173c1
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-07-23 16:07:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -6,35 +6,65 @@ using namespace std;
 class Solution
 {
 	public:
-	void dfs(int node,vector<int> &vis, vector<int> adj[],stack<int> &st){
-	    if(vis[node] == 1){
-	        return;
-	    }
-	    vis[node] = 1;
-	    for(auto it: adj[node]){
-	        if(!vis[it]){
-	            dfs(it,vis,adj,st);
-	        }
-	    }
-	    st.push(node);
-	}
+// 	void dfs(int node,vector<int> &vis, vector<int> adj[],stack<int> &st){
+// 	    if(vis[node] == 1){
+// 	        return;
+// 	    }
+// 	    vis[node] = 1;
+// 	    for(auto it: adj[node]){
+// 	        if(!vis[it]){
+// 	            dfs(it,vis,adj,st);
+// 	        }
+// 	    }
+// 	    st.push(node);
+// 	}
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
 	    vector<int> vis(V,0);
-	    stack<int> st;
-	    
-	    for(int i =0;i<V;i++){
-	        if(!vis[i]){
-	            dfs(i,vis,adj,st);
+	    //stack<int> st;
+	    queue<int> q;
+	    vector<int> d(V,0);
+	    vector<int> res;
+	    //cal indegrees
+	    for(int i = 0; i<V;i++){
+	        for(auto it: adj[i]){
+	            d[it]++;
 	        }
 	    }
-	    vector<int> res;
-	    for(int i=0; i<V;i++){
-	        res.push_back(st.top());
-	        st.pop();
+	    //pushing nodes with 0 degree
+	    for(int i = 0; i<V;i++){
+	        if(d[i] == 0){
+	            q.push(i);
+	        }
 	    }
+	    
+	    while(!q.empty()){
+	        int n = q.size();
+	        
+	        for(int i = 0; i<n;i++){
+	            int node = q.front(); q.pop();
+	            
+	            for(auto it: adj[node]){
+	                d[it]--;
+	                if(d[it] == 0) q.push(it);
+	            }
+	            
+	            res.push_back(node);
+	        }
+	    }
+	    
+	   // for(int i =0;i<V;i++){
+	   //     if(!vis[i]){
+	   //         dfs(i,vis,adj,st);
+	   //     }
+	   // }
+	   // vector<int> res;
+	   // for(int i=0; i<V;i++){
+	   //     res.push_back(st.top());
+	   //     st.pop();
+	   // }
 	    return res;
 	}
 };
