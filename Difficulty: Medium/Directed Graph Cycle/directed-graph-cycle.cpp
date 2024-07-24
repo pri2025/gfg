a8be25fbb497488314965271f30849e77c8265d3
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-07-24 19:30:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -6,58 +6,61 @@ using namespace std;
 class Solution {
   public:
   
-    // bool dfs(int node, vector<int> &vis,vector<int> adj[]){
-    //     if(vis[node] == 1){
-    //         return true;
-    //     }
-    //     vis[node] = 1;
-    //     for(auto it: adj[node]){
-    //         if(!vis[it]){
-    //             dfs(it,vis,adj);
-    //         }
-    //         else{
-    //             return true;
-    //         }
+    bool dfs(int node, vector<int> &vis, vector<int> &pathvis, vector<int> adj[]){
+        if(vis[node] == 1 && pathvis[node] == 1){
+            return true;
+        }
+        vis[node] = 1;
+        pathvis[node] = 1;
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                if (dfs(it,vis,pathvis,adj)) return true;
+            }
+            else if(pathvis[it] == 1){
+                return true;
+            }
             
-    //     }
-    //     return false;
-    // }
+        }
+        pathvis[node] = 0;
+        return false;
+    }
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        vector<int> d(V,0);
-        queue<int> q;
-        for(int i=0; i<V;i++){
-            for(auto it: adj[i]){
-                d[it]++;
-            }
-        }
-        for(int i = 0; i<V;i++){
-            if(d[i] == 0) q.push(i);
-        }
-        
-        while(!q.empty()){
-            int n =q.size();
-            
-            for(int i= 0; i<n;i++){
-                int node = q.front(); q.pop();
-                for(auto it: adj[node]){
-                    d[it]--;
-                    if(d[it] == 0) q.push(it);
-                }
-                
-            }
-        }
-        
-         for(int i = 0; i<V;i++){
-            if(d[i] != 0) return true;
-        }
-        //vector<int> vis(V,0);
-        // for(int i = 0; i<V; i++){
-        //     if(!vis[i]){
-        //       if(dfs(i,vis,adj)) return true;
+        //vector<int> d(V,0);
+        // queue<int> q;
+        // for(int i=0; i<V;i++){
+        //     for(auto it: adj[i]){
+        //         d[it]++;
         //     }
         // }
+        // for(int i = 0; i<V;i++){
+        //     if(d[i] == 0) q.push(i);
+        // }
+        
+        // while(!q.empty()){
+        //     int n =q.size();
+            
+        //     for(int i= 0; i<n;i++){
+        //         int node = q.front(); q.pop();
+        //         for(auto it: adj[node]){
+        //             d[it]--;
+        //             if(d[it] == 0) q.push(it);
+        //         }
+                
+        //     }
+        // }
+        
+        //  for(int i = 0; i<V;i++){
+        //     if(d[i] != 0) return true;
+        // }
+        vector<int> vis(V,0);
+        vector<int> pathvis(V,0);
+        for(int i = 0; i<V; i++){
+            if(!vis[i]){
+              if(dfs(i,vis,pathvis,adj)) return true;
+            }
+        }
         return false;
     }
 };
