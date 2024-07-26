dba3a857828b013e81b99d0292578c087736ae0c
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-07-26 14:14:46 +0530

    Time: 0 ms (100%), Space: 12.2 MB (9.48%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,38 +11,11 @@
  */
 class Solution {
 public:
-    // void fn2(TreeNode* root2, vector<int> &in2){
-    //     if(root2 == NULL){
-    //         return;
-    //     }
-
-    //     in2.push_back(root2->val);
-    //     fn2(root2->left,in2);
-    //     fn2(root2->right,in2);
-    // }
-    // void fn(TreeNode* root1,vector<int> &in1){
-    //     if(root1 == NULL){
-    //         return;
-    //     }
-
-    //     in1.push_back(root1->val);
-    //     fn(root1->left,in1);
-    //     fn(root1->right,in1);
-    // }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        //vector<int>in1, in2;
-        if(p == NULL && q == NULL) return true;
+        if(p == nullptr && q == nullptr) return true;
         if(p == nullptr || q == nullptr) return false;
         if(p->val != q->val) return false;
 
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-
-        // fn(p,in1);
-        // fn2(q,in2);
-        // if(in1 == in2){
-        //     return true;
-        // }
-        // return false;
-        return true;
+        return isSameTree(p->left, q->left) && isSameTree(p->right,q->right);
     }
 };
