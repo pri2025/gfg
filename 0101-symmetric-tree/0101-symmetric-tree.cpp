c1eb41dc5c7087ea3ed777a8ba44cedc3ae79b8b
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-07-26 15:30:25 +0530

    Time: 3 ms (70.89%), Space: 17.6 MB (10.62%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,25 +11,27 @@
  */
 class Solution {
 public:
-    TreeNode* invert(TreeNode* root){
-        if(root == nullptr) return root;
+    // TreeNode* invert(TreeNode* root){
+    //     if(root == nullptr) return root;
 
-        TreeNode* left = invert(root->left);
-        TreeNode* right = invert(root->right);
-        root->left = right;
-        root->right = left;
-        return root;
-    }
+    //     TreeNode* left = invert(root->left);
+    //     TreeNode* right = invert(root->right);
+    //     root->left = right;
+    //     root->right = left;
+    //     return root;
+    // }
     bool same(TreeNode* p, TreeNode* q){
         if(p == nullptr && q == nullptr) return true;
         if(p == nullptr || q == nullptr) return false;
-        if(p->val != q->val) return false;
-        return same(p->left, q->left) && same(p->right ,q->right);
+        return (p->val == q->val) && same(p->left,q->right) && same(p->right , q->left);
+        //return same(p->left, q->left) && same(p->right ,q->right);
     }
     bool isSymmetric(TreeNode* root) {
+        if(root == nullptr) return true;
+        return same(root->left,root->right);
         //TreeNode* root1 = root;
-        TreeNode* newroot = invert(root->left);//sirf ek side invert kro
-        return same(root->right,newroot);//dono subtrees compare kro if same
+        //TreeNode* newroot = invert(root->left);//sirf ek side invert kro
+       // return same(root->right,newroot);//dono subtrees compare kro if same
         
     }
 };
