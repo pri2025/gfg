97ce3e6a1228598b800fac902a5ef8cd9782527a
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-01 11:23:00 +0530

    Time: 16 ms (9.72%), Space: 21.4 MB (29.84%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,49 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        //int n = nums.size();
-        if(nums.size() == 1){
-            return 1;
-        }
-        int j =1;
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i] == nums[i+1]){
-
-            }
+        int i = 0 , j = 1;
+        while(j <nums.size()){
+            if(nums[i] == nums[j]) j++;
             else{
-                nums[j] = nums[i+1];
-                j++;
+                nums[i+1] = nums[j];
+                i++;
             }
         }
-        return j;
-
-        // int j = 1;
-        // if(n==1){
-        // return 1;
-        // }
-        // for(int i = 1; i<n ; i++){
-        //     if(nums[i] != nums[i-1]){
-        //         nums[j] = nums[i];
-        //         j++;
-        //     }
-        // }
-        // return j;
-
-
-
-
-
-
-        
+        return i+1;
     }
 };
-
-//int j = 1;
-        // for(int i= 0; i<nums.size()-1; i++){
-        //     if(nums[i] != nums[i+1]){
-        //         nums[j] = nums[i+1];
-        //         j++;
-        //     }
-           
-        // }
-        // return j;
