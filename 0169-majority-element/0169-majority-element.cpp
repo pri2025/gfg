ba9e4ea1765d05f9f5447bbe7216cb692829c923
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-01 13:13:45 +0530

    Time: 13 ms (84.26%), Space: 26.9 MB (50.56%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -11,21 +11,13 @@ public:
             else if(curr == nums[i]) cnt++;
             else cnt--;
         }
-        // vector<int> hash;
+
         int cnt2=0;
         for(int it : nums){
             if(it == curr) cnt2++;
         }
         if(cnt2 >= n/2) return curr;
         return -1;
-       
-        // for(int it: nums){
-        //     hash[it]++;
-        // }
-        // int maxi = 0;
-        // for(int i = 0; i<hash.size()+1;i++){
-        //     maxi = max(maxi,hash[i]);
-        // }
-        // return maxi;
+      
     }
 };
