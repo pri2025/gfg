adf90998db0a0499ce65af091b3d930cd41e11b9
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-01 15:10:15 +0530

    Time: 24 ms (28.73%), Space: 27.6 MB (53.21%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
+    void fn(vector<int> &nums, int s, int e){
+        while(s<=e){
+            swap(nums[s],nums[e]);
+            s++; e--;
+        }
+    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        vector<int> arr(n);
+        fn(nums,0,n-k-1);
+        fn(nums,n-k, n-1);
+        fn(nums,0,n-1);
 
-        for(int i = 0; i<n; i++){
-            arr[(i+k) % n] = nums[i];
-        }
+       // vector<int> arr(n);
 
-        for(int i = 0; i<n; i++){
-            nums[i] = arr[i];
-        }
+        // for(int i = 0; i<n; i++){
+        //     arr[(i+k) % n] = nums[i];
+        // }
+
+        // for(int i = 0; i<n; i++){
+        //     nums[i] = arr[i];
+        // }
 
     }
 };
