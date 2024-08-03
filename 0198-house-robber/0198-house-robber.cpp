8bbf1f8f82709750f6a94c0a12b26f43f4896933
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-03 17:58:58 +0530

    Time: 0 ms (100%), Space: 9.4 MB (38.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -19,15 +19,20 @@ public:
         // vector<int>memo(n,-1);
         // return fn(0,nums,memo);
 
-        vector<int>dp(n,-1);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        //vector<int>dp(n,-1);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
+
+        int prev2 = nums[0], prev = max(nums[0], nums[1]);
 
         for(int i= 2;i<n;i++){
-            int pick = nums[i] + dp[i-2];
-            int notpick = dp[i-1];
-            dp[i] = max(pick, notpick);
+            // int pick = nums[i] + dp[i-2];
+            // int notpick = dp[i-1];
+            int pick = nums[i] + prev2;
+            int notpick = prev;
+            int curr = max(pick, notpick);
+            prev2 = prev; prev = curr;
         }
-        return dp[n-1];
+        return prev;
     }
 };
