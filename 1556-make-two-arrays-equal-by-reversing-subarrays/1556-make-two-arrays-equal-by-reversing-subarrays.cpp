f260352e1f8edf17dea62443aa7cdef82702e0b5
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-03 18:13:57 +0530

    Time: 6 ms (86.43%), Space: 17.7 MB (34.48%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        int n= target.size();
-        sort(target.begin(), target.end());
-        sort(arr.begin(), arr.end());
-        int i =0, j=0;
-        while(i<n){
-            if(target[i] != arr[j]){
-                return false;
-            }
-            i++;j++;
+        int n= target.size(); int maxi =INT_MIN;
+        vector<int> hash(1001,0);
+        for(int i=0; i<n;i++){
+            hash[target[i]]++;
+            hash[arr[i]]--;
+
+            maxi  =max({maxi,target[i],arr[i]});
+
         }
+
+        for(int i=0; i<=maxi;i++){
+            if(hash[i] !=0) return false;
+        }
+        return true;
+        // sort(target.begin(), target.end());
+        // sort(arr.begin(), arr.end());
+        // int i =0, j=0;
+        // while(i<n){
+        //     if(target[i] != arr[j]){
+        //         return false;
+        //     }
+        //     i++;j++;
+        // }
         return true;
     }
 };
