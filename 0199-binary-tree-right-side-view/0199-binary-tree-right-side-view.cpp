123855369bb1d28f0110883f7b91b832e9b05f8b
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-05 22:13:39 +0530

    Time: 0 ms (100%), Space: 14.2 MB (59.51%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,32 +11,41 @@
  */
 class Solution {
 public:
+    void fn(TreeNode* root, int level, vector<int> &ans){
+        if(root == nullptr) return;
+
+        if(ans.size() == level) ans.push_back(root->val);
+        fn(root->right, level+1,ans);
+        fn(root->left,level+1,ans);
+    }
     vector<int> rightSideView(TreeNode* root) {
-        vector<vector<int>> res;
+        // vector<vector<int>> res;
         vector<int> ans;
         if(root == nullptr) return ans;
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()){
-            int n = q.size();
-            vector<int> level;
-
-            for(int i =0; i<n;i++){
-                TreeNode* node = q.front(); q.pop();
-                level.push_back(node->val);
-
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            res.push_back(level);
-        }
-        
-        for(int i =0; i<res.size();i++){
-            int m = res[i].size();
-            ans.push_back(res[i][m-1]);
-        }
+        fn(root,0,ans);
         return ans;
+        // queue<TreeNode*> q;
+        // q.push(root);
+
+        // while(!q.empty()){
+        //     int n = q.size();
+        //     vector<int> level;
+
+        //     for(int i =0; i<n;i++){
+        //         TreeNode* node = q.front(); q.pop();
+        //         level.push_back(node->val);
+
+        //         if(node->left) q.push(node->left);
+        //         if(node->right) q.push(node->right);
+        //     }
+        //     res.push_back(level);
+        // }
+        
+        // for(int i =0; i<res.size();i++){
+        //     int m = res[i].size();
+        //     ans.push_back(res[i][m-1]);
+        // }
+        // return ans;
 
         
     }
