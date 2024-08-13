a516f24e2f4bae20aa91e475e56e830a6a208932
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-13 23:25:59 +0530

    Time: 7 ms (72.36%), Space: 20.2 MB (35.33%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int fn(int indx, int a,vector<int> &coins,vector<vector<int>> &memo){
+        if(a == 0) return 1;
+        if(indx == 0){
+            
+            return (a % coins[indx]==0);
+            
+        }
+        if(memo[indx][a] != -1) return memo[indx][a];
+        int pick = 0;
+        if(coins[indx] <= a){
+            pick = fn(indx,a-coins[indx],coins,memo);
+        }
+        int notpick = fn(indx-1,a,coins,memo);
+        return memo[indx][a] = pick+notpick;
+    }
+    int change(int a, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> memo(n,vector<int>(a+1,-1));
+        return fn(n-1,a,coins,memo);
+    }
+}; 
