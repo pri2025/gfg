d3ea5a20d086ac3623066fa2ede6f635e237fc0c
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-14 16:31:36 +0530

    Time: 49 ms (29.47%), Space: 46.8 MB (41.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int fn(int indx,int a, vector<int>&coins,vector<vector<int>> &memo){
-       // if(a == 0) return INT_MAX-1;
-        if(indx== 0){
-            if(a % coins[indx] == 0){
-                return a/coins[indx];
-            }
-            else{
-                return INT_MAX-1;
-            }
-        }
-        if(memo[indx][a] != -1)return memo[indx][a];
-        int pick = INT_MAX;
-        if(coins[indx] <= a){
-            pick = 1 + fn(indx,a-coins[indx],coins,memo);
-        }
-        int notpick = fn(indx-1,a,coins,memo);
-        return memo[indx][a] = min(pick,notpick);
-    }
+    // int fn(int indx,int a, vector<int>&coins,vector<vector<int>> &memo){
+    //    // if(a == 0) return INT_MAX-1;
+    //     if(indx== 0){
+    //         if(a % coins[indx] == 0){
+    //             return a/coins[indx];
+    //         }
+    //         else{
+    //             return INT_MAX-1;
+    //         }
+    //     }
+    //     if(memo[indx][a] != -1)return memo[indx][a];
+    //     int pick = INT_MAX;
+    //     if(coins[indx] <= a){
+    //         pick = 1 + fn(indx,a-coins[indx],coins,memo);
+    //     }
+    //     int notpick = fn(indx-1,a,coins,memo);
+    //     return memo[indx][a] = min(pick,notpick);
+    // }
     int coinChange(vector<int>& coins, int a) {
         if(a == 0) return 0;
         int n = coins.size();
@@ -29,11 +29,35 @@ public:
                 return -1;
             }
         }
-        vector<vector<int>> memo(n,vector<int>(a+1,-1));
+        // vector<vector<int>> memo(n,vector<int>(a+1,-1));
 
-        int res = fn(n-1,a,coins,memo);
-        if(res == INT_MAX-1) return -1;
-        else return res;
+        // int res = fn(n-1,a,coins,memo);
+        // if(res == INT_MAX-1) return -1;
+        // else return res;
+        
+        vector<vector<int>> dp(n,vector<int>(a+1,-1));
+
+        for(int i = 0 ; i<=a;i++){
+            if(i%coins[0] == 0){
+                dp[0][i] = i/coins[0];
+            }
+            else{
+                dp[0][i] = INT_MAX-1;
+            }
+        }
+
+        for(int i = 1; i<n;i++){
+            for(int j =0;j<=a;j++){
+                int pick = INT_MAX;
+                if(coins[i]<=j){
+                    pick = 1+dp[i][j-coins[i]];
+                }
+                int notpick = dp[i-1][j];
+                dp[i][j] = min(pick,notpick);
+            }
+        }
+        if(dp[n-1][a] == INT_MAX-1) return -1;
+        else return dp[n-1][a];
         
 
     }
