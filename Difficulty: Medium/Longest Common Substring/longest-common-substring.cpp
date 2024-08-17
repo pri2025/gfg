07caa0cfec2e92971b4986e70333b10a6638ed8c
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-17 11:37:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -6,47 +6,38 @@ using namespace std;
 // } Driver Code Ends
 
 class Solution {
-    public:
+  public:
     int longestCommonSubstr(string str1, string str2) {
         // your code here
-        vector<vector<int>> dp(str1.size(), vector<int> (str2.size(), -1));
-        int maxS = 0;
-        for(int i=0; i<str1.size(); i++){
-            for(int j=0; j<str2.size(); j++){
-                if(str1[i] == str2[j] && dp[i][j] == -1){
-                    int k=i, l=j;
-                    while(k<str1.size() && l<str2.size() && str1[k] == str2[l]){
-                        dp[k][l] = 1;
-                        k++;
-                        l++;
-                    }
-                    maxS = max(maxS, l-j);
+        int n = str1.length();
+        int m = str2.length();
+        if(n==0 || m ==0) return 0;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        for(int i =0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i =0;i<=m;i++){
+            dp[0][i]=0;
+        }
+        for(int i = 1; i<=n; ++i){
+            for(int j = 1; j <=m; ++j){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 0;
                 }
             }
         }
-        return maxS;
+        int maxi = 0;
+        for(int i=0; i<=n;i++){
+            for(int j=0;j<=m;j++){
+                maxi=max(maxi,dp[i][j]);
+            }
+        }
+        return maxi;
     }
 };
-// class Solution {
-//   public:
-//     int fn(string &x, string &y, int n, int m){
-//         if(n<0 || m<0) return 0;
-
-//         if(x[n] == y[m]) return 1+fn(x,y,n-1,m-1);
-//         else{
-//             return max(fn(x,y,n-1,m), fn(x,y,n,m-1));
-//         }
-//         return 0;
-//     }
-//     int longestCommonSubstr(string str1, string str2) {
-//         // your code here
-//         int n = str1.length();
-//         int m = str2.length();
-//         if(n==0 || m ==0) return 0;
-//         //vector<vector<int>> memo(n,vector<int>(m,0))
-//         return fn(str1,str2,n-1,m-1); 
-//     }
-//};
 
 //{ Driver Code Starts.
 
