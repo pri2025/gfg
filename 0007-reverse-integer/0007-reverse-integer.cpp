cb9b57c4ca7ae874aab6e00739d96f47858d1872
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-27 22:15:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (36.75%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int num = x;
+        int digit;
+        int newn = 0;
+        int bl = 0;
+        if(num < 0){
+            num = abs(num);
+            bl = 1;
+        }
+        while(num>0){
+            digit = num % 10;
+            if(newn > INT_MAX / 10) return 0; 
+            newn = newn*10 + digit;
+            num = num/10;
+        }
+        
+        if(bl == 0){
+            return newn;
+        }
+        return newn*(-1);
+    }
+};
