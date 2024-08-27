02bfbab77262d6d99dabd49e21f5a74b02816451
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-27 15:24:58 +0530

    Time: 388 ms (69.84%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select w.id
 from Weather w inner join Weather ww
-on datediff(w.recordDate , ww.recordDate) = 1 and
+on subdate(w.recordDate ,1) =  ww.recordDate and
 w.temperature > ww.temperature;
