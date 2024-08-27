d10d8306143b27f5fcaaf0c04da4317cec864c4c
Priyansha Mahajan <149517138+pri2025@users.noreply.github.com>
2024-08-27 23:26:50 +0530

    Time: 22 ms (43.84%), Space: 27.6 MB (47.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -9,9 +9,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        fn(nums,0,n-k-1);
-        fn(nums,n-k, n-1);
         fn(nums,0,n-1);
+        fn(nums,0, k-1);
+        fn(nums,k,n-1);
 
        // vector<int> arr(n);
 
